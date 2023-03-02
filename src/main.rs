43efd1196a18b7fa5c2e85ea1fea551260aee630
procrastimax <mmeheykeroth@protonmail.com>
2023-03-02 19:34:39 +0100

    Implements RGB LED for most ESP32 DevKit Boards
    The used LED on my board (ESP32-C3-DevKitM-1) is a WS2812 and is
    controlled via RMT. This commit integrates the code from
    https://github.com/esp-rs/espressif-trainings/blob/main/common/lib/esp32-c3-dkc02-bsc/src/led.rs
    into the most recent version of esp_idf_sys with only some changes to
    some used c_types and the addition of setting a GPIO Pin for the LED
    when calling ::new()

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,35 @@
 use esp_idf_sys as _; // If using the `binstart` feature of `esp-idf-sys`, always keep this module imported
 
+use std::{thread, time::Duration};
+
+mod led;
+
+use led::{RGB8, WS2812RMT};
+
 fn main() {
     // It is necessary to call this function once. Otherwise some patches to the runtime
     // implemented by esp-idf-sys might not link properly. See https://github.com/esp-rs/esp-idf-template/issues/71
     esp_idf_sys::link_patches();
 
-    println!("Hello, world!");
+    println!("Turning RGB LED light on!");
+
+    let mut rgb_led = WS2812RMT::new(8).expect("RGB LED should be creatable!");
+
+    rgb_led.set_pixel(RGB8::new(255, 0, 0)).unwrap();
+    thread::sleep(Duration::from_secs(3));
+
+    rgb_led.set_pixel(RGB8::new(0, 255, 0)).unwrap();
+    thread::sleep(Duration::from_secs(3));
+
+    rgb_led.set_pixel(RGB8::new(0, 0, 255)).unwrap();
+    thread::sleep(Duration::from_secs(3));
+
+    rgb_led.set_pixel(RGB8::new(255, 255, 255)).unwrap();
+    thread::sleep(Duration::from_secs(3));
+
+    rgb_led.set_pixel(RGB8::new(90, 20, 200)).unwrap();
+    thread::sleep(Duration::from_secs(3));
+
+    println!("Turning RGB LED light off!");
+    rgb_led.set_pixel(RGB8::new(0, 0, 0)).unwrap();
 }
